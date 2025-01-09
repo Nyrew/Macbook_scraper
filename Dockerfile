56038eb350fe7c82431e924baff228fb61068be7
Nyrew <lukas.vokel@gmail.com>
2025-01-09 20:01:27 +0100

    docker file updated

--- Dockerfile
+++ Dockerfile
@@ -8,14 +8,46 @@ COPY requirements.txt .
 
 RUN pip install --no-cache-dir -r requirements.txt
 
+# Nainstalování závislostí pro Playwright
+RUN apt-get update && apt-get install -y \
+    libgobject-2.0-0 \
+    libglib2.0-0 \
+    libnss3 \
+    libnssutil3 \
+    libsmime3 \
+    libnspr4 \
+    libdbus-1-3 \
+    libatk1.0-0 \
+    libatk-bridge2.0-0 \
+    libcups2 \
+    libgio2.0-0 \
+    libdrm2 \
+    libexpat1 \
+    libxcb1 \
+    libxkbcommon0 \
+    libatspi2.0-0 \
+    libx11-6 \
+    libxcomposite1 \
+    libxdamage1 \
+    libxext6 \
+    libxfixes3 \
+    libxrandr2 \
+    libgbm1 \
+    libpango-1.0-0 \
+    libcairo2 \
+    libasound2 \
+    --no-install-recommends \
+    && apt-get clean \
+    && rm -rf /var/lib/apt/lists/*
+
+# Instalace Playwright a jeho prohlížečů
+RUN pip install playwright && playwright install
+
 # Zkopírování celé aplikace do kontejneru
 COPY . .
 
 # Nastavení proměnného prostředí pro Playwright (instalace prohlížečů)
 ENV PLAYWRIGHT_SKIP_BROWSER_DOWNLOAD=0
-
-# Instalace prohlížečů pro Playwright
-RUN playwright install
 
 # Exponování portu, na kterém běží aplikace
 EXPOSE 8000
--- tests/test2.py
+++ tests/test2.py
@@ -1,22 +1,14 @@
 import requests
 
-headers = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36 Avast/131.0.0.0",
-    "Accept": "application/json, text/javascript, */*; q=0.01",
-    "X-Requested-With": "XMLHttpRequest",
-    "Referer": "https://istyle.cz/macbook-air-15-3-apple-m3-8jadrove-cpu-10jadrove-gpu-16gb-512gb-ssd-cz-vesmirne-sedy.html",
-}
+#url = "https://web-scraper-ihnl.onrender.com/test-selenium"
+url = "https://docker-scraper-573o.onrender.com/scrape" # docker - render
+#url = "http://localhost:8000/scrape" # docker - local
 
-url = "https://istyle.cz/ajaxcaptainhook/ajaxhook/productpagehook/"
-params = {
-    "product_id": "159045",
-    "url": "https://istyle.cz/macbook-air-15-3-apple-m3-8jadrove-cpu-10jadrove-gpu-16gb-512gb-ssd-cz-vesmirne-sedy.html",
-    "data[]": "firstload",
-}
-
-response = requests.get(url, headers=headers, params=params)
-
-print(response.status_code)
-print(response)
-data = response.json()
-print(data["price"]["price"])
+try:
+    response = requests.post(url)
+    response.raise_for_status()  # Vyvolá chybu, pokud HTTP status není 200
+    data = response.json()
+    print("Response from Render Selenium endpoint:")
+    print(data)
+except requests.exceptions.RequestException as e:
+    print(f"Error connecting to Selenium endpoint: {e}")
